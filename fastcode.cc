58a53f57f07e93e55367641018afe5856128c863
lee23 <71886857+schummLee@users.noreply.github.com>
2023-10-02 10:06:37 -0700

    Update fastcode.cc

--- fastcode.cc
+++ fastcode.cc
@@ -17,3 +17,25 @@ auto sum(T arg1, Args... args) -> decltype(sum_impl(arg1, args...)) {
     static_assert((std::is_same_v<T, Args> && ...), "All types must be the same!");
     return sum_impl(arg1, args...);
 }
+#include <type_traits>
+#include <new> 
+
+template<typename T1, typename T2>
+class MyVariant {
+public:
+    MyVariant() { currentIndex = -1; }    
+    ~MyVariant() { reset(); }
+
+    void set(const T1& value) { reset(); new (&t1) T1(value); currentIndex = 0; }
+    void set(const T2& value) { reset(); new (&t2) T2(value); currentIndex = 1; }
+    int index() const { return currentIndex; }
+
+    T1 get_as_T1() const { if (currentIndex != 0) { throw std::runtime_error("Wrong type"); } return t1; }
+
+    T2 get_as_T2() const { if (currentIndex != 1) { throw std::runtime_error("Wrong type"); } return t2; }
+
+private:
+    void reset() { if (currentIndex == 0) { t1.~T1(); } else if (currentIndex == 1) { t2.~T2(); } currentIndex = -1; }
+    union { T1 t1; T2 t2; };
+    int currentIndex;
+};
