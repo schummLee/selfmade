ececfc67f7323b2f90ab226753cce6b38917acdf
lee23 <71886857+schummLee@users.noreply.github.com>
2023-11-13 16:33:40 +0100

    Add files via upload

---
+++ artist_model.hpp
@@ -0,0 +1,10 @@
+#ifndef ARTIST_MODEL.HPP
+#define ARTIST_MODEL.HPP
+
+extern long long return_artist_PID();
+
+extern std::string return_artist_name();
+
+extern std::vector<std::pair<long long,std::string>> album_PID();
+
+#endif
