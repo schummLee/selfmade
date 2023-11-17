a8b89de371eeec837ba0844ef26ed44f3026bbfc
lee23 <71886857+schummLee@users.noreply.github.com>
2023-11-17 03:13:32 +0100

    Add files via upload

--- syscall_kernel.hpp
+++ syscall_kernel.hpp
@@ -2,3 +2,13 @@
 
 noreturn _syscall_call_return_kernel_thread_kill_or_suspend();
 
+
+// thanks to Amith32_joi posting the new frame-standrad so we can
+// modified new session and avert old ones
+
+extern void initalized_info();
+
+extern inline bool is_return();
+
+
+
