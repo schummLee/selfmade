4c905ecbf5e7ed9c90b812c04f2b41464b824eb1
lee23 <71886857+schummLee@users.noreply.github.com>
2024-02-06 07:29:31 +0100

    Update ColorInverter.cpp

--- color-inverse/src/function/ColorInverter.cpp
+++ color-inverse/src/function/ColorInverter.cpp
@@ -1,1 +1,14 @@
+#include "ColorInverter.h"
+
+ColorInverter::ColorInverter() 
+{
+    // Constructor initialization if needed
+}
+
+void ColorInverter::InvertColor(cv::Mat& frame) 
+{
+    // Invert the color of the frame
+    cv::bitwise_not(frame, frame);
+}
+
 
