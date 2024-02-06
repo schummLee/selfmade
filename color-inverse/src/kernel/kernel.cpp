cad260ed5c55b492224c079085876cea05ccb7dc
lee23 <71886857+schummLee@users.noreply.github.com>
2024-02-07 00:15:05 +0100

    Update kernel.cpp

--- color-inverse/src/kernel/kernel.cpp
+++ color-inverse/src/kernel/kernel.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <chrono>
 #include "../functions/VideoProcessor.h"
+#include "../functions/GrayscaleConverter.h"
+#include "../functions/EdgeDetector.h"
+#include "../functions/BlurApplier.h"
+#include "../functions/ThresholdApplier.h"
+#include "../functions/HistogramEqualizer.h"
+#include "../functions/Rotator.h"
+#include "../functions/Resizer.h"
+#include "../functions/TextOverlay.h"
+#include "../functions/FrameSaver.h"
+
 
 void kernel_as_multithreading_enaplied_as_decentralized() 
 {
     // Initialize VideoProcessor with video file path and desired FPS
     VideoProcessor videoProcessor("input_video.mp4", 30.0);
 
-    // Initialize processing components here
-
+    // Initialize processing components
+    GrayscaleConverter grayscaleConverter;
+    EdgeDetector edgeDetector;
+    BlurApplier blurApplier;
+    ThresholdApplier thresholdApplier;
+    HistogramEqualizer histogramEqualizer;
+    Rotator rotator;
+    Resizer resizer;
+    TextOverlay textOverlay;
+    FrameSaver frameSaver;
 
     // Set dynamic parameters (optional)
     videoProcessor.SetDesiredFPS(60.0);
