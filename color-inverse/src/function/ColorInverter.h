420ba8e72c87ba1d83d98eb34f1f55e520d9d239
lee23 <71886857+schummLee@users.noreply.github.com>
2024-02-06 07:28:47 +0100

    Update ColorInverter.h

--- color-inverse/src/function/ColorInverter.h
+++ color-inverse/src/function/ColorInverter.h
@@ -1,1 +1,20 @@
+#ifndef COLOR_INVERTER_H
+#define COLOR_INVERTER_H
+
+#include <opencv2/opencv.hpp>
+
+class ColorInverter 
+{
+public:
+    ColorInverter();
+
+    // Function to invert the color of a single frame
+    void InvertColor(cv::Mat& frame);
+
+private:
+    // Add any private member variables or functions as needed
+};
+
+#endif // COLOR_INVERTER_H
+
 
