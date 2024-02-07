fa9b662a9fd41faf9f2d3acbb6fbfbe4560d0254
lee23 <71886857+schummLee@users.noreply.github.com>
2024-02-08 00:27:32 +0100

    Update ColorInverter.cpp

--- color-inverse/src/function/ColorInverter.cpp
+++ color-inverse/src/function/ColorInverter.cpp
@@ -1,9 +1,10 @@
 #include "ColorInverter.h"
 
-ColorInverter::ColorInverter() 
+ColorInverter::ColorInverter(cv::Mat& inverted_color_swap) : inverted_color_swap(inverted_color_swap) 
 {
-    // Constructor initialization if needed
+    // Constructor body (if any)
 }
+
 
 void ColorInverter::InvertColor(cv::Mat& frame) 
 {
@@ -11,4 +12,16 @@ void ColorInverter::InvertColor(cv::Mat& frame)
     cv::bitwise_not(frame, frame);
 }
 
-
+void ColorInverter::InvertColor_BlacknWhite(cv::Mat& frame)
+{
+    // Iterate through each pixel in the image
+    for (int y = 0; y < frame.rows; ++y) {
+        for (int x = 0; x < frame.cols; ++x) {
+            // Get the intensity value of the pixel
+            uchar& intensity = frame.at<uchar>(y, x);
+            
+            // Invert the intensity value
+            intensity = 255 - intensity;
+        }
+    }
+}
