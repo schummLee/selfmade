cfe8d71cc70932e3e794599c0c7b4a5bcd83fe15
lee23 <71886857+schummLee@users.noreply.github.com>
2024-02-15 06:32:31 +0100

    Update use_basic.cc

--- color-inverse/example/use_basic.cc
+++ color-inverse/example/use_basic.cc
@@ -1,3 +1,8 @@
 #include <cmath>
 
 // new 
+// include all header here 
+// this will be show as an example in here
+// any form of new data stream will come through test first then before become a example
+
+// set the resolution of 1920 1080 only and default
